41594ddd2746d27367b5ec38a8a29a7f309562e3
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-10-09 21:57:46 +0200

    unit 2 is completed

---
+++ 2. JavaScript Essentials/2.3/index.js
@@ -0,0 +1,5 @@
+a = prompt("Enter a: ");
+b = prompt("Enter b: ");
+//c**2 =a**2+b**2
+c = Math.sqrt(a ** 2 + b ** 2);
+alert(`c = ${c}`);
---
+++ 2. JavaScript Essentials/2.4/index.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 20;
+let c = 30;
+a += b;
+a /= c;
+c = c % b;
+console.log(a, b, c);
